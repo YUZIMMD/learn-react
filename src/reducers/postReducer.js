915ab4b2a828aaabf5394b4821a5908b070195fb
dengyt <2248545525@qq.com>
2018-11-04 18:13:01 +0800

    拆分reducer

---
+++ src/reducers/postReducer.js
@@ -0,0 +1,10 @@
+// reducer 的作用返回新的状态
+const initialState ={
+    items:[]
+}
+export default function (state= initialState,action){
+    switch(action.type){
+        default:
+        return state;
+    }
+}
--- src/store.js
+++ src/store.js
@@ -1,7 +1,11 @@
 import {createStore,applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
+import rootReducer from './reducers/index'
 // 参数：reducer，state，中间件
 const initialState = {};
+// 组成数组，一步步执行中间件
 const middleware = [thunk];
- const store = createStore(()=>[],initialState,applyMiddleware(...middleware))
+// reducer：store收到action之后必须给出一个新的state，这样view才会发生变化，这种state的计算过程叫reducer，reducer是一个函数，它接受Action和当前state作为参数返回以一个新的state
+//  reducer的拆分
+const store = createStore(rootReducer,initialState,applyMiddleware(...middleware))
 export default store;
